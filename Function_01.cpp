58b2e0c8e85e923c1bda5fd0c9a0529497f3cfcc
Ankit Panghal <ankitpanghal452@gmail.com>
2026-08-19 22:58:56 +0530

    ....

--- Function_01.cpp
+++ Function_01.cpp
@@ -1,18 +1,43 @@
-#include<iostream>
+// #include<iostream>
+// using namespace std;
+// int add(int a, int b){
+//     return a+b;
+// }
+// int main()
+// {
+//     int x;
+//     cout<<"Enter the number : ";
+//     cin>>x;
+//     int y;
+//     cout<<"Enter the number : ";
+//     cin>>y;
+//     int result;
+//     result=add(x,y);
+//     cout<<"Sum of two numbers is : "<<result<<endl;
+//     return 0;
+// }
+
+
+
+
+#include <iostream>
 using namespace std;
-int add(int a, int b){
-    return a+b;
+
+int square(int n)
+{
+    return n * n;
 }
+
 int main()
 {
-    int x;
-    cout<<"Enter the number : ";
-    cin>>x;
-    int y;
-    cout<<"Enter the number : ";
-    cin>>y;
-    int result;
-    result=add(x,y);
-    cout<<"Sum of two numbers is : "<<result<<endl;
+    int num;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    int result = square(num);
+
+    cout << "Square = " << result;
+
     return 0;
 }
